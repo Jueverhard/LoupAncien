68123da4084d31a21493a5b564a6c4944c3f18fb
Julien Everhard <jueverhard@hotmail.fr>
2020-02-09 11:10:10 +0100

    Prise en compte du PREFIX + adaptation des timeout vis-à-vis de discord v12

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Client } = require("discord.js");
-const { TOKEN } = require("./config");
+const { TOKEN, PREFIX } = require("./config");
 const bot = new Client();
 
 
@@ -15,43 +15,43 @@ const testChannelID = '556237949214392342';
 
 //Permet de savoir lorsque le bot est opérationnel lors de son lancement
 bot.on('ready', function(){
-    console.log("I'm there !");
+  console.log("I'm there !");
 })
 
 
 
 //Récupère la liste des membres ayant réagis avec un certain emoji
 bot.on('messageUpdate', message => {
-    console.log(message);
-    if(reaction.message.id == idTestMessage && reaction.emoji.name === '✅') {
-        
-        let reactionUsersPromise = reaction.users.fetch();
-        reactionUsersPromise.then(function(value) {
-            console.log(value);
-        })
+	console.log(message);
+	if(reaction.message.id == idTestMessage && reaction.emoji.name === '✅') {		
+		let reactionUsersPromise = reaction.users.fetch();
+		reactionUsersPromise.then(function(value) {
+			console.log(value);
+		})
 
-        
-        
-        /*for(var user in reactionUsers) {
-            user
-        }*/
-    }
+		/*for(var user in reactionUsers) {
+			user
+		}*/
+	}
 })
 
 //Envoie une réponse pré-formatée à un message précis, puis supprime la réponse et le message original
-bot.on('message', message => {
-    console.log(idTestMessage);
-    if(message.content === '!ping'){
-        message.reply('pong !').then(sent => {
-            sent.delete(5000);
-            message.delete(5000);
-        });
+bot.on('message', msg => {
+  // Ne réagis pas si l'auteur du message est un bot
+  if (msg.author.bot) return;
+  // Récupère chaque argument dans un tableau (ex : "${PREFIX}cmd lol mdr" => {"${PREFIX}cmd", "lol", "mdr"})
+  const args = msg.content.split(/ +/g);
+  // R2cupère le premier élément (la commande) et assure qu'elle soit en minuscule
+  // ex : {"${PREFIX}CmD", "lol", "mdr"} => {"${PREFIX}cmd"}
+  const cmd = args.shift().toLowerCase();
+	if(cmd === `${PREFIX}ping`){
+		msg.reply('pong !').then(sent => {
+			sent.delete({"timeout": 5000});
+			msg.delete({"timeout": 5000});
+			});
     }
-    if (message.content === '!react') {
-        //message.react(':white_check_mark:');
-        setTimeout(function(value) {
-            console.log(message.reactions);
-        }, 5000)
+    if (cmd === `${PREFIX}react`) {
+        msg.react('✅');
     }
 
 
