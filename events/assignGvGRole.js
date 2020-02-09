e299d287816d85f74f62feb515c2e37c2c9e5235
Julien Everhard <jueverhard@hotmail.fr>
2020-02-09 20:07:37 +0100

    Idée pour le développement du script

--- events/assignGvGRole.js
+++ events/assignGvGRole.js
@@ -2,9 +2,16 @@ module.exports = (client) => {
   const serverDamnedWolves = client.guilds.resolve(client.DamnedWolvesServerID);
   const GvGRole = serverDamnedWolves.roles.get(client.GvGRoleID);
 
-  client.channels.fetch(client.RulesChanelID).then(chan => {
-    chan.messages.fetch(client.RulesMsgID).then(msg => {
+
+  // Maybe use setInterval() to determine when to check for date and time ?
+  // Then if it is right, look at the reactions, then send an embed message to remind the participants
+  // setInterval("alert(('bip')", 10000);
+
+
+  client.channels.fetch(client.GvGChanelID).then(chan => {
+    chan.messages.fetch().then(msg => {
       const checkFilter = (reaction) => reaction.emoji.name === '✅';
+      const xFilter = (reaction) => reaction.emoji.name === '❌';
       const collector = msg.createReactionCollector(checkFilter);
       collector.on('collect', r => {
         serverDamnedWolves.members.fetch().then(membersCollection => {
