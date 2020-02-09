60106aa5e5c4ba9ae614629724368675aff2787e
Julien Everhard <jueverhard@hotmail.fr>
2020-02-09 23:41:49 +0100

    La commande gvgquestion est prête à être utilisée (par tout le monde à l'heure actuelle : A CHANGER)

--- commands/gvgquestion.js
+++ commands/gvgquestion.js
@@ -2,21 +2,23 @@ const { MessageEmbed } = require("discord.js");
 var moment = require("moment");
 
 module.exports = (client, message) => {
+  message.delete({ timeout: 5000 });
   const yesAnswer = '✅';
   const noAnswer = '❌';
   const date1 = moment().add(7, 'days').startOf('isoweek').format('DD/MM');
-  const date2 = moment().add(7, 'days').startOf('isoweek').add(6, 'days').format('DD/MM');
+  const date2 = moment().add(7, 'days').startOf('isoweek').add(5, 'days').format('DD/MM');
   const date3 = moment().add(7, 'days').startOf('isoweek').subtract(1, 'days').format('DD/MM');
   const embed = new MessageEmbed()
     .setTitle(`Qui veut participer aux GvG de la semaine du ${date1} au ${date2} ?`)
-    .addField("Si tu es dispo et chaud pour te battre, clique sur : ", yesAnswer)
+    .setDescription("@everyone Comme prévu, voici le message hebdomadaire pour vous incrire aux prochaines Guerres de Guilde !")
+    .addField("Si tu es dispo prêt à te battre, clique sur : ", yesAnswer)
     .addField("Si non, clique sur : ", noAnswer)
     .setFooter(`Vous avez jusqu'au ${date3} à 22h pour répondre.`);
 
-    client.channels.fetch(client.LogsChanelID).then(chan => {
-      chan.send(embed).then(async msg => {
-        await msg.react(yesAnswer);
-        await msg.react(noAnswer);
-      })
+  client.channels.fetch(client.GvGChanelID).then(chan => {
+    chan.send(embed).then(async msg => {
+      await msg.react(yesAnswer);
+      await msg.react(noAnswer);
     })
+  })
 };
