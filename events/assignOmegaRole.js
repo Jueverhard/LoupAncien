ceeb850fc3588fddf8143016ab23a01d52914c9a
Julien Everhard <jueverhard@hotmail.fr>
2020-02-09 20:48:14 +0100

    Modification légère du message et note d'un TODO

--- events/assignOmegaRole.js
+++ events/assignOmegaRole.js
@@ -6,16 +6,35 @@ module.exports = (client) => {
     chan.messages.fetch(client.RulesMsgID).then(msg => {
       const checkFilter = (reaction) => reaction.emoji.name === '✅';
       const collector = msg.createReactionCollector(checkFilter);
+      // msg.awaitReactions(checkFilter, { time: 15000 })
+      //   .then(collected => console.log(collected));
       collector.on('collect', r => {
         serverDamnedWolves.members.fetch().then(membersCollection => {
           for (m of membersCollection.values()) {
             if (r.users.some(u => u.id === m.user.id)) {
-              if (!m.roles.find(r => r === omegaRole)) m.roles.add(omegaRole);
+              if (!m.roles.find(r => r === omegaRole)) {
+                m.roles.add(omegaRole);
+                m.send(`Tu as désormais le rôle ${omegaRole.name} ! Fais-en bon usage, et n'oublie pas les règles que tu viens d'accepter :grin:`);
+              }
             }
           }
         });
       });
-      collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+      
+      // TODO : Get back the omegaRole when a member removes its reaction to the rules
+
+      // collector.on('dispose', r => {
+      //   console.log("Something happened");
+      //   serverDamnedWolves.members.fetch().then(membersCollection => {
+      //     for (m of membersCollection.values()) {
+      //       if (r.users.some(u => u.id === m.user.id)) {
+      //         if (m.roles.find(r => r === omegaRole)) m.roles.remove(omegaRole);
+      //       }
+      //     }
+      //   });
+      // });
+
+      // collector.on('end', collected => console.log(`Collected ${collected.size} items`));
     })
   });
 };
