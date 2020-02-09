94c2b24d29ea533ac3049a34e9865d23176348d7
Julien Everhard <jueverhard@hotmail.fr>
2020-02-09 21:06:17 +0100

    MAJ de refresh pour coller à assignGvGRole

--- commands/refresh.js
+++ commands/refresh.js
@@ -1,10 +1,12 @@
 module.exports = (client, message, args) => {
   const serverDamnedWolves = client.guilds.resolve(client.DamnedWolvesServerID);
-  const omegaRole = serverDamnedWolves.roles.get(client.OmegaRoleID);
+
 
   message.delete({ timeout: 3000});
 
   if (args[0] === "omega") {
+    const omegaRole = serverDamnedWolves.roles.get(client.OmegaRoleID);
+    
     client.channels.fetch(client.RulesChanelID).then(c => {
       c.messages.fetch(client.RulesMsgID).then(m => {
         m.reactions.resolve('✅').users.fetch().then( usersCollection => {
@@ -19,6 +21,32 @@ module.exports = (client, message, args) => {
       })
     });
   } else if (args[0] === "gvg") {
-    // TODO
+    const GvGRole = serverDamnedWolves.roles.get(client.GvGRoleID);
+    const NoGvGRole = serverDamnedWolves.roles.get(client.NoGvGRoleID);
+    
+    client.channels.fetch(client.GvGChanelID).then(chan => {
+      chan.messages.fetch(/* TODO : get the last EmbedMessage inviting to subscribe to GVG */).then(msg => {
+        msg.reactions.resolve('✅').users.fetch().then( usersCollection => {
+          serverDamnedWolves.members.fetch().then(membersCollection => {
+            for (m of membersCollection.values()) {
+              if (usersCollection.some(u => u.id === m.user.id)) {
+                if (!m.roles.find(r => r === GvGRole)) m.roles.add(GvGRole);
+                if (m.roles.find(r => r === NoGvGRole)) m.roles.remove(NoGvGRole);
+              }
+            }
+          })
+        })
+        msg.reactions.resolve('❌').users.fetch().then( usersCollection => {
+          serverDamnedWolves.members.fetch().then(membersCollection => {
+            for (m of membersCollection.values()) {
+              if (usersCollection.some(u => u.id === m.user.id)) {
+                if (!m.roles.find(r => r === NoGvGRole)) m.roles.add(NoGvGRole);
+                if (m.roles.find(r => r === GvGRole)) m.roles.remove(GvGRole);
+              }
+            }
+          })
+        })
+      })
+    });
   }
 };
