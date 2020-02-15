ba1f2240ddacf2fc1147b8282924b757f876718f
Julien Everhard <jueverhard@hotmail.fr>
2020-02-15 19:09:01 +0100

    Modification de gvganswer pour afficher les membres présent ou non en gvg

--- commands/gvganswer.js
+++ commands/gvganswer.js
@@ -11,18 +11,36 @@ module.exports = (client, message) => {
   const date1 = moment().add(7, 'days').startOf('isoweek').format('DD/MM');
   const date2 = moment().add(7, 'days').startOf('isoweek').add(5, 'days').format('DD/MM');
   const date3 = moment().add(7, 'days').startOf('isoweek').subtract(1, 'days').format('DD/MM');
+  var gvgMembers = [];
+  var noGvgMembers = [];
+
+  setTimeout(function(){
+    gvgMembers = gvgMembers.filter(m => m != "Loup ancien");
+    noGvgMembers = noGvgMembers.filter(m => m != "Loup ancien");
+    const embed = new MessageEmbed()
+      .setTitle(`Suite aux réponses des membres pour les GvG de la semaine du ${date1} au ${date2}, vos rôles ont été mis à jour`)
+      .setDescription("Ceux qui souhaitent participer ont désormais le rôle GvG\nCeux qui ne le veulent pas ou ne sont pas disponibles ont, eux, le rôle NoGvG")
+      .addField("N'oubliez pas de faire vos attaques tous les ", GvGRole);
+    if (gvgMembers.length > 0) embed.addField("GvG : ", gvgMembers);
+    if (noGvgMembers.length > 0) embed.addField("NoGvG : ", noGvgMembers)
+
+    client.channels.fetch(client.GvGChanelID).then(chan => {
+      chan.send(embed);
+    });
+  }, 5000);
 
   client.channels.fetch(client.GvGChanelID).then(chan => {
     chan.messages.fetch().then(msgCollections => {
       for(msg of msgCollections.values()){
         if (msg.pinned && msg.embeds.length == 1) {
           if (msg.embeds[0].footer.text === `Vous avez jusqu'au ${date3} à 22h pour répondre.`) {
-            msg.reactions.resolve('✅').users.fetch().then( usersCollection => {
+            msg.reactions.resolve('✅').users.fetch().then(usersCollection => {
               serverDamnedWolves.members.fetch().then(membersCollection => {
                 for (m of membersCollection.values()) {
                   if (usersCollection.some(u => u.id === m.user.id)) {
                     if (!m.roles.find(r => r === GvGRole)) m.roles.add(GvGRole);
                     if (m.roles.find(r => r === NoGvGRole)) m.roles.remove(NoGvGRole);
+                    gvgMembers.push(m.user.username);
                   }
                 }
               })
@@ -33,23 +51,16 @@ module.exports = (client, message) => {
                   if (usersCollection.some(u => u.id === m.user.id)) {
                     if (!m.roles.find(r => r === NoGvGRole)) m.roles.add(NoGvGRole);
                     if (m.roles.find(r => r === GvGRole)) m.roles.remove(GvGRole);
+                    noGvgMembers.push(m.user.username);
                   }
                 }
               })
             })
             msg.unpin();
           }
-        } 
+        }
       }
     })
   });
 
-  const embed = new MessageEmbed()
-    .setTitle(`Suite aux réponses des membres pour les GvG de la semaine du ${date1} au ${date2}, vos rôles ont été mis à jour`)
-    .setDescription("Ceux qui souhaitent participer ont désormais le rôle GvG\nCeux qui ne le veulent pas ou ne sont pas disponibles ont, eux, le rôle NoGvG")
-    .addField("N'oubliez pas de faire vos attaques tous les ", GvGRole);
-
-  client.channels.fetch(client.GvGChanelID).then(chan => {
-    chan.send(embed);
-  });
 };
