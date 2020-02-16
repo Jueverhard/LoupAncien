b199068e4c78c6446270720cec6c3358539cc630
Julien Everhard <jueverhard@hotmail.fr>
2020-02-16 23:07:30 +0100

    Désactivation de l'attribution des rôles GVG/NoGVG au clic, pour éviter de taguer les mauvaises personnes en cours de semaines

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -4,10 +4,10 @@ module.exports = (client, reaction, user, tools) => {
 	const date = moment().add(7, 'days').startOf('isoweek').subtract(1, 'days').format('DD/MM');
 	if (reaction.message.id === client.RulesMsgID) {
 		tools.removeRole(client, user.id, client.OmegaRoleID)
-	} else if (reaction.message.pinned && reaction.message.footer === `Vous avez jusqu'au ${date} à 22h pour répondre.`) {
-		if (reaction.emoji === '✅')
-			tools.removeRole(client, user.id, client.GvGRoleID);
-		else if (reaction.emoji === '❌')
-			tools.removeRole(client, user.id, client.NoGvGRoleID);
+	// } else if (reaction.message.pinned && reaction.message.footer === `Vous avez jusqu'au ${date} à 22h pour répondre.`) {
+	// 	if (reaction.emoji === '✅')
+	// 		tools.removeRole(client, user.id, client.GvGRoleID);
+	// 	else if (reaction.emoji === '❌')
+	// 		tools.removeRole(client, user.id, client.NoGvGRoleID);
 	}
 };
