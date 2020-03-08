ef514be15e1661061356c19150bdba364ca97c7b
Julien Everhard <jueverhard@hotmail.fr>
2020-03-08 11:58:11 +0100

    Ajout d'un log pour dire que le bot a MP un membre ayant rejoint le serveur

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,6 @@
 module.exports = (client, member) => {
   member.send(`Salut et bienvenue ${member} ! Pense à jeter un coup d'oeil aux règles :wink:`);
+  client.channels.fetch(client.LogsChanelID).then(chan => {
+    chan.send(`${member.displayName} a rejoint le serveur, je lui ai envoyé un MP pour lui souhaiter la bienvenue et l'inviter à lire les règles !`)
+  })
 };
