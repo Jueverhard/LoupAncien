7c8c8c02386b653655eefe426096dc0df63179c5
Julien Everhard <jueverhard@hotmail.fr>
2020-03-14 19:38:04 +0100

    Ajout de la possibilité de répéter le message dans un channel différent de celui de la commande

--- commands/repeat.js
+++ commands/repeat.js
@@ -1,5 +1,13 @@
 module.exports = (client, message, args) => {
-  message.channel.send(args.join(" "));
-  console.log(`Le message '${message.content}' va être supprimé.`)
-  message.delete({ timeout: 3000 }).then(console.log("Le message a été supprimé"));
+  chanToSend = args.shift().toLowerCase();
+  if (chanToSend == "here") {
+    message.channel.send(args.join(" "));
+  } else {
+    for (chan of client.channels.values()) {
+      if (chan.name.toLowerCase().includes(chanToSend)) {
+        chan.send(args.join(" "));
+      }
+    }
+  }
+  message.delete({ timeout: 1000});
 };
