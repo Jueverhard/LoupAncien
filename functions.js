ef7697fc9ea0773ae481600cc3bdccb7a27f85ba
Julien Everhard <jueverhard@hotmail.fr>
2020-03-18 18:45:44 +0100

    Fix des méthodes addRole() et removeRole()

--- functions.js
+++ functions.js
@@ -3,30 +3,30 @@ module.exports = {
 	addRole: function(client, userID, roleID) {
 		const role = client.guilds.resolve(client.DamnedWolvesServerID).roles.get(roleID);
 		client.guilds.resolve(client.DamnedWolvesServerID).members.fetch().then(membersCollection => {
-			for (m of membersCollection.values()) {
+			membersCollection.each(m => {
 				if (m.user.id === userID) {
 					if (!m.roles.find(r => r === role)) {
 						m.roles.add(role);
 						m.send(`Tu as désormais le rôle ${role.name} ! Fais-en bon usage, et n'oublie pas les règles qui vont avec :eyes:`);
 					}
 				}
-			}			
-		})
+			})
+		});
 	},
 
 	// Remove the role of id roleID from the user of id userID
 	removeRole: function(client, userID, roleID) {
 		const role = client.guilds.resolve(client.DamnedWolvesServerID).roles.get(roleID);
 		client.guilds.resolve(client.DamnedWolvesServerID).members.fetch().then(membersCollection => {
-			for (m of membersCollection.values()) {
+			membersCollection.each(m => {
 				if (m.user.id === userID) {
 					if (m.roles.find(r => r === role)) {
 						m.roles.remove(role);
 						m.send(`Tu n'as plus le rôle ${role.name} !`);
 					}
 				}
-			}		
-		})
+			})	
+		});
 	},
 
 	// Put the message of id messageID and in the chanel of id chanelID in cache
