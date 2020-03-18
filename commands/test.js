df8f4b8af7213fb6031f2e6a9a83b047d01766ad
Julien Everhard <jueverhard@hotmail.fr>
2020-03-18 19:15:28 +0100

    Suppression du message après exécution de la commande test

--- commands/test.js
+++ commands/test.js
@@ -2,4 +2,5 @@ module.exports = (client, message) => {
   client.channels.fetch(client.GvGChanelID).then(chan => {
     chan.messages.each(m => console.log(m));
   });
+  message.delete({ timeout: 1000});
 };
