9d4b6a53845cc2b86f4865ba4e4b56c123bd9c9a
Julien Everhard <jueverhard@hotmail.fr>
2020-06-05 02:49:24 +0200

    Modification du ready.js afin de fetch précisément le message de gvgquestion (à l'origine du fix de gvganswer)

--- events/ready.js
+++ events/ready.js
@@ -2,13 +2,24 @@ var moment = require("moment");
 
 module.exports = (client, tools) => {
   console.log("I'm there !");
+  client.GvGQuestionMsgID;
   const serverDamnedWolves = client.guilds.resolve(client.DamnedWolvesServerID);
+  const date = moment().add(7, 'days').startOf('isoweek').subtract(1, 'days').format('DD/MM');
 
   tools.fetchMessage(client, client.RulesChanelID, client.RulesMsgID);
 
   serverDamnedWolves.roles.fetch();
 
   client.channels.fetch(client.GvGChanelID).then(chan => {
-    chan.messages.fetchPinned();
+    chan.messages.fetchPinned().then(pinnedMessages => {
+      for(msg of pinnedMessages.values()) {
+        if (msg.embeds.length == 1) {
+          if (msg.embeds[0].footer.text === `Vous avez jusqu'au ${date} à 22h pour répondre.`) {
+            client.GvGQuestionMsgID = msg.id;
+          }
+        }
+      }
+    })
   })
+  tools.fetchMessage(client, client.GvGChanelID, client.GvGQuestionMsgID);
 };
