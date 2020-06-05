6f1a2326b6b2f74be66e5e635a72c6775da32c1b
Julien Everhard <jueverhard@hotmail.fr>
2020-06-05 15:45:45 +0200

    Mise à jour du message d'help selon la personne qui l'utilise

--- commands/help.js
+++ commands/help.js
@@ -3,9 +3,9 @@ const { MessageEmbed } = require("discord.js");
 module.exports = (client, message) => {
   let texte = "Ce message détaille les différentes commandes reconnues par le bot et utilisables lorsque celui-ci est " +
               "connecté. Servez-vous-en avec bienveillance ou vous ne pourrez plus vous en servir !\n\n" +
-              `-"**${client.PREFIX}help**" : Liste les commandes reconnues par le bot (c\'est-à-dire envoie ce message) ;\n` +
-              `-"**${client.PREFIX}bj5**" : Fournit un tableur permettant de vérifier la validité de sa team BJ5 ;\n` +
-              `-"**${client.PREFIX}refresh omega**" : Met à jour les rôles Omega selon les personnes ayant réagis au message des règles ;\n` +
+              `-"**${client.PREFIX}bj5**" : Fournit un tableur permettant de vérifier la validité de sa team BJ5 ;\n`;
+  if (client.moreThanOmegaMembersID.some(e => e == message.author.id)) {
+    texte +=  `-"**${client.PREFIX}refresh omega**" : Met à jour les rôles Omega selon les personnes ayant réagis au message des règles ;\n` +
               `-"**${client.PREFIX}repeat *nom_d\'un_channel* *et_là_tu_peux_dire_un_peu_ce_que_tu_veux***" : Fait répéter au bot, dans le ` +
               `channel indiqué (ou dans le channel du message si *here*) ce qui suit le nom du channel ;\n` +
               `-"**${client.PREFIX}gvg**" : Envoie un message dans le salon **guerre-de-guilde** pour rappeler aux membres y participant ` +
@@ -17,8 +17,10 @@ module.exports = (client, message) => {
               `-"**${client.PREFIX}laby *un_nom* *un_autre_nom***" : Envoie un message privé aux membres listés pour leur rappeler ` +
               `d\'utiliser leurs lanternes dans le labyrinthe. Vous pouvez mettre autant de noms que vous le voulez ;\n` +
               `-"**${client.PREFIX}gvganswer**" : Disponible uniquement le Dimanche soir à partir de 22h, permet de mettre à jour les rôles ` +
-              `GVG (et NoGVG) et liste les personnes souhaitant participer aux Guerre de Guilde et la semaine à venir.`;
-
+              `GVG (et NoGVG) et liste les personnes souhaitant participer aux Guerre de Guilde et la semaine à venir ;`;
+  }
+  texte += `-"**${client.PREFIX}help**" : Liste les commandes reconnues par le bot (c\'est-à-dire envoie ce message).\n`;
+              
   const embed = new MessageEmbed()
     .setTitle(`Commandes reconnues par Amarok`)
     .setDescription(texte)
