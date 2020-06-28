ee15876b82e3262ec8ae1f15399ee28311e84913
Julien Everhard <jueverhard@hotmail.fr>
2020-06-28 23:23:21 +0200

    Fix du ready suite au changement du footer

--- events/ready.js
+++ events/ready.js
@@ -14,7 +14,7 @@ module.exports = (client, tools) => {
     chan.messages.fetchPinned().then(pinnedMessages => {
       for(msg of pinnedMessages.values()) {
         if (msg.embeds.length == 1) {
-          if (msg.embeds[0].footer.text === `Vous avez jusqu'au ${date} à 22h pour répondre.`) {
+          if (msg.embeds[0].footer.text === `Vous avez jusqu'au ${date} à 22h pour répondre. S'il y a moins de 10 inscrits, des personnes n'ayant pas demander à ne pas participer seront ajoutées pour compléter.`) {
             client.GvGQuestionMsgID = msg.id;
             console.log(`ID du message de gvgquestion : ${client.GvGQuestionMsgID}`)
           }
